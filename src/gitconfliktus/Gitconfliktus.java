1b161e5107677598575cb14eb9df69ad4d91f252
Erdős Viktória <erdos.viktoria@akk.lan>
2026-09-17 15:53:09 +0200

    csere

--- src/gitconfliktus/Gitconfliktus.java
+++ src/gitconfliktus/Gitconfliktus.java
@@ -4,8 +4,8 @@ package gitconfliktus;
 public class Gitconfliktus {
 
     public static void main(String[] args) {
-        System.out.println("Viki");
         System.out.println("Márk");
+        System.out.println("Viki");
     }
     
 }
